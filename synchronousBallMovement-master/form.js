7021c2fcc6ea0946321f5e46f70f96f66e6c9e27
gaandeev <gaandeevkrishnamalla@gmail.com>
2021-05-21 15:56:11 +0530

    first commit

---
+++ synchronousBallMovement-master/form.js
@@ -0,0 +1,55 @@
+class Form{
+    constructor(){
+
+    }
+
+    display(){
+
+        var title = createElement("h2");
+        title.html("CAR RACING GAME");
+        title.position(130,0);
+
+        var input=createInput("name");
+        input.position(130,160);
+
+        var button = createButton("play");
+        button.position(250,200);
+
+        button.mousePressed(function(){
+input.hide();
+button.hide();
+var name=input.value();
+playerCount+=1;
+player.update(name);
+player.updateCount(playerCount);
+var greeting = createElement('h3');
+ greeting.html("Hi " + name ) 
+ greeting.position(130, 160)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        });
+
+    }
+}
